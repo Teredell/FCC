2dd2169b3de50b14449917feffc55da3b4028c54
Teredell <44662148+Teredell@users.noreply.github.com>
2020-05-28 15:36:09 +0800

    Create Lesson-021.js

---
+++ Lesson-021.js
@@ -0,0 +1,9 @@
+//Less on 21 - Declare String VariablesPassed
+
+//Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = "Mister";
+
+var myLastName = "Teredell";
+
+console.log(myFirstName + " " + myLastName);
