78a0205437d150d4be49bcf2e641d9a982bd2299
Teredell <44662148+Teredell@users.noreply.github.com>
2020-06-15 15:58:51 +0800

    Busy day, only managed to add 3 today

---
+++ Lesson-086.js
@@ -0,0 +1,18 @@
+//Lesson 86 - Delete Properties from a JavaScript Object
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+
+  console.log(myDog);
+
+  delete myDog.tails;
+
+  console.log(myDog);
