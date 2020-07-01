68d936bf03c3cbc9c26dfec566a7b7af87ec6c94
Teredell <44662148+Teredell@users.noreply.github.com>
2020-07-01 14:46:28 +0800

    Create Lesson-102.js

---
+++ Lesson-102.js
@@ -0,0 +1,13 @@
+//Lesson 102 - Generate Random Fractions with JavaScript
+
+function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
+  
+console.log(randomFraction()); //Returns value between 0.0000000000000000 & 0.9999999999999999
+  
