7122a81fdfc40bc507698bfdfac9f98f5df9a1b0
Teredell <44662148+Teredell@users.noreply.github.com>
2020-07-19 11:11:32 +0800

    Update Lesson-008.js

--- ES6/Lesson-008.js
+++ ES6/Lesson-008.js
@@ -5,8 +5,8 @@ const increment = (number, value = 1) => number + value;
 // Only change code above this line
 
 console.log(increment(5, 4));
-
 console.log(increment(5));
+
 
 const greeting = (name = "Anonymous") => "Hello " + name;
 
