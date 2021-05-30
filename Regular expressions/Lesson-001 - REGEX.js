ce07bef4107a214b47c113398835e7e7e2fd39ac
Teredell <44662148+Teredell@users.noreply.github.com>
2021-05-30 16:29:20 +0800

    From home

---
+++ Regular expressions/Lesson-001 - REGEX.js
@@ -0,0 +1,10 @@
+// Lesson 001 - Using the Test Method
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+//let result = myRegex; // Change this line - This is how the line was oringially
+let result = myRegex.test(myString); // This is the answer
+
+console.log(result); //if above worked console should show True.
+
+
--- Regular expressions/Regular expressions README.md
+++ Regular expressions/Regular expressions README.md
@@ -1,1 +1,3 @@
 I will document my journey through the REGEX exercises on FCC in this file
+
+So REGEX is searching through strings using diffeent methodolgies.
