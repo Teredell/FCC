e2c3d2609c17133f6e82e57e96eedfac5abd0609
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-18 16:17:42 +0800

    Update Lesson-026 - REGEX.js

--- Regular expressions/Lesson-026 - REGEX.js
+++ Regular expressions/Lesson-026 - REGEX.js
@@ -5,4 +5,4 @@ let haRegex = /change/; // Change this line - This is how the line was oringiall
 let haRegex = /Haz{4,}ah/; // Change this line
 let result = haRegex.test(haStr);
 
-console.log(result) //// Console will output True if string haRegex has between 4 or more z's, in this case True
+console.log(result); // Console will output True if string haRegex has between 4 or more z's, in this case True
