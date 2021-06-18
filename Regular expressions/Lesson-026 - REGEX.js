9fe8b17252c06e69c0f74cd4cb1cf827144ecc21
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-18 15:27:48 +0800

    Create Lesson-026 - REGEX.js

---
+++ Regular expressions/Lesson-026 - REGEX.js
@@ -0,0 +1,8 @@
+//Lesson 026 - Specify Only the Lower Number of Matches
+
+let haStr = "Hazzzzah";
+let haRegex = /change/; // Change this line - This is how the line was oringially
+let haRegex = /Haz{4,}ah/; // Change this line
+let result = haRegex.test(haStr);
+
+console.log(result) //// Console will output True if string haRegex has between 4 or more z's, in this case True
