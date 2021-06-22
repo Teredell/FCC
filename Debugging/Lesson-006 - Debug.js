40183ada21d2d99360e4d71b989eff14aee7f50a
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-22 12:43:58 +0800

    Create Lesson-006 - Debug.js

---
+++ Debugging/Lesson-006 - Debug.js
@@ -0,0 +1,6 @@
+//Lesson 006 - Catch Mixed Usage of Single and Double Quotes
+
+//let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>"; - before correcting
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+
+console.log(innerHtml); // Will display correctly
