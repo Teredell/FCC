66653dd664945065bd03825073c8a6cbb83a0270
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-22 11:27:36 +0800

    Create Lesson-004 - Debug.js

---
+++ Debugging/Lesson-004 - Debug.js
@@ -0,0 +1,7 @@
+//Lesson 004 - Catch Misspelled Variable and Function Names
+
+let receivables = 10;
+let payables = 8;
+//let netWorkingCapital = recievables - payable; //Prior to changes
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`); //Will now work correctly & display: Networking capital is: 2
