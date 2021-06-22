5b0fa25da586d17246c250acab79af4f81e006b3
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-22 11:13:25 +0800

    Create Lesson-003 - Debug.js

---
+++ Debugging/Lesson-003 - Debug.js
@@ -0,0 +1,8 @@
+//Lesson 003 - Use typeof to Check the Type of a VariablePassed
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven); //Should display number
+console.log(typeof three); //Should display string
