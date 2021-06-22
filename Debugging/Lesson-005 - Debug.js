5f5f707ca19b2d864d49fa1b526fb99ce1c9f4b6
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-22 11:54:26 +0800

    Create Lesson-005 - Debug.js

---
+++ Debugging/Lesson-005 - Debug.js
@@ -0,0 +1,8 @@
+//Lesson 005 - Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+//let myArray = [1, 2, 3; // before fixes
+let myArray = [1, 2, 3];
+//let arraySum = myArray.reduce((previous, current =>  previous + current); // before fixes
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`); // Should display: Sum of array values is: 6
+
