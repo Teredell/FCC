afbdcd415421864908e572eb94bf061d7bbd9075
Teredell <44662148+Teredell@users.noreply.github.com>
2021-06-24 11:51:54 +0800

    Create Lesson-001 - ES6.js

---
+++ ES6/Lesson-001 - ES6.js
@@ -0,0 +1,14 @@
+//Lesson 001 - Explore Differences Between the var and let Keywords
+
+//var catName; //Previous incorrect
+//var quote; //Previous incorrect
+let catName; //Corrected for lesson
+let quote; //Corrected for lesson
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
