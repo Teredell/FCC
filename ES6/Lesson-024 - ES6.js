4e5c469c4ccf09f8a822a007a3335c776bcd6de3
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-01 11:43:07 +0800

    Create Lesson-024 - ES6.js

---
+++ ES6/Lesson-024 - ES6.js
@@ -0,0 +1,9 @@
+//Lesson 024 - Reuse JavaScript Code Using import
+
+import {uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+console.log(uppercaseString("hello"));
