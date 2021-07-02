62a7c62b864a4e4859674e4a5f3cd1b613ec1deb
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-02 15:32:00 +0800

    Create Lesson-002 - BDS.js

---
+++ Basic Data Structures/Lesson-002 - BDS.js
@@ -0,0 +1,7 @@
+//Lesson 002 - Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "Star Wars";
+// Only change code above this line
+console.log(myArray);
