9261b3b2bf0a1d3420be372c006656ba814f0cf4
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-07 11:51:14 +0800

    Create Lesson-013 - BDS.js

---
+++ Basic Data Structures/Lesson-013 - BDS.js
@@ -0,0 +1,16 @@
+//Lesson 013 - Add Key-Value Pairs to JavaScript Objects
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+foods.grapes = 35;
+foods['strawberries'] = 27;
+let someFruit = 'bananas';
+foods[someFruit] = 13;
+// Only change code above this line
+
+console.log(foods);
