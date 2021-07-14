5e49686f237806a46191ed1eafd7cd23d0200d4d
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-14 14:17:26 +0800

    Update Lesson-015 - BAS.js

--- 006 - Basic Algorithm Scripting/Lesson-015 - BAS.js
+++ 006 - Basic Algorithm Scripting/Lesson-015 - BAS.js
@@ -2,7 +2,7 @@
 
 function mutation(arr) {
   let arrayTestingAgainst = arr[0].toLowerCase();
-  let arrayTestingFor = arr[1];
+  let arrayTestingFor = arr[1].toLowerCase();
   
   for(let counter = 0; counter < arrayTestingFor.length; counter++){
     if(arrayTestingAgainst.indexOf(arrayTestingFor[counter]) < 0){
