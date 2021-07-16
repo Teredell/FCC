53e996b0c7ae8fc582692ada35555f3b9be6dfef
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-16 09:55:29 +0800

    Create Lesson-002 - OOP.js

---
+++ 007 - Object Oriented Programming/Lesson-002 - OOP.js
@@ -0,0 +1,11 @@
+//Lesson 002 - Use Dot Notation to Access the Properties of an Object
+
+let dog = {
+  name: "Woofy", 
+  numLegs: 3, 
+  numWheels: 1
+};
+// Only change code below this line
+console.log("My dog's name is: " + dog.name + ".");
+console.log("My dog has " + dog.numLegs + " legs.");
+console.log("My dog has " + dog.numWheels + " wheels.");
