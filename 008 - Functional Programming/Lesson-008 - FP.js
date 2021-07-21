f9c536c32c2f7d42a5ea6b0e5d9402b97bc976f4
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-21 13:22:33 +0800

    Create Lesson-008 - FP.js

---
+++ 008 - Functional Programming/Lesson-008 - FP.js
@@ -0,0 +1,20 @@
+//Lesson 008 - Implement map on a Prototype
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // Only change code below this line
+  for(let x = 0; x < this.length; x++){
+    newArray.push(this[x] * 2);
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+console.log(new_s);
