5a2aa10c5617e2414ee3fe9d63b28a06a8a8b929
Teredell <44662148+Teredell@users.noreply.github.com>
2021-07-29 11:04:13 +0800

    Create Lesson-022 - FP.js

---
+++ 008 - Functional Programming/Lesson-022 - FP.js
@@ -0,0 +1,11 @@
+//Lesson 022 - Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+  // Only change code below this line
+  let someBool = arr.every(function(positivityCheck){
+    return positivityCheck > 0;
+  })
+  return someBool;
+  // Only change code above this line
+}
+console.log(checkPositive([1, 2, 3, -4, 5]));
