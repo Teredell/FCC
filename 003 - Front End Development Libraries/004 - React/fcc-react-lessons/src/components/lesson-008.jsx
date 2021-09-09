6098c8f712fd790bdeb2f621eee1dcdad144da7f
Teredell <44662148+Teredell@users.noreply.github.com>
2021-09-09 16:02:18 +0800

    minor edits

--- 003 - Front End Development Libraries/004 - React/fcc-react-lessons/src/components/lesson-008.jsx
+++ 003 - Front End Development Libraries/004 - React/fcc-react-lessons/src/components/lesson-008.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
-//function Lesson008(props) {
-//  const MyComponent = <div>
-//  <Link to="/">Landing</Link><br /><br /><br />
-//    <div>Some string of text</div>
-//  </div>
-//  return (MyComponent);
-//}
+//Refer lesson 7 for default page look
 
 class Lesson008 extends React.Component {
 //   constructor(props) {   //For the purposes of this exercise, the constructor isn't needed in this webpage but will be later
